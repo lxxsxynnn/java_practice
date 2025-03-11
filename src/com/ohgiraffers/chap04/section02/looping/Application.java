4d3d235199de30fa2a17eae553fd9ebbc6ec2697
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-03-11 13:55:40 +0900

    반복문과 분기문 -1 강의 필기 정리

---
+++ src/com/ohgiraffers/chap04/section02/looping/Application.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.chap04.section02.looping;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        A_for a = new A_for();
+        /* 단독 for문 흐름 확인용 메소드 호출 */
+        //a.testSimpleForStatement();
+
+        /* 반복문을 사용하는 예제(필요성 느끼기) */
+        //a.testForExample1();
+        //a.testForExample2();
+        //a.testForExample3();
+        //a.testForExample4();
+
+        /* 반복문을 일상 생활에서 사용하는 예제(규칙 찾기) */
+        //a.printSimpleGugudan();
+
+        /* 중첩 반복문을 사용하는 예제 */
+
+        /* 단독 while문 흐름 확인용 메소드 호출 */
+
+        /* 반복문을 사용하는 예제(for문과 비교) */
+
+        /* 단독 do-while문 흐름 확인용 메소드 호출 */
+
+        /* do-while 반복문을 사용하는 에제 */
+    }
+}
